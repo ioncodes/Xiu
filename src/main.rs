d6dc4ae6e3f4869078206884162256cda846d23f
Luca <ionix@protonmail.com>
2018-06-05 17:26:05 +0200

    added cmd line switch for verbose mode (print disasm)

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ mod registers;
 mod instructions;
 mod cpu;
 
+use std::env;
 use cpu::CPU;
 
 fn main() {
-    let mut cpu = CPU::new(String::from("DMG_ROM.bin"), true);
+    let args: Vec<_> = env::args().collect();
+    let mut verbose = false;
+    if args.contains(&String::from("-v")) {
+        verbose = true;
+    }
+    let mut cpu = CPU::new(String::from("DMG_ROM.bin"), verbose);
 
     cpu.run();
 }
